e493bff1bec65137a6798043306f967668797c52
andrewimwilliams <andrewimwilliams@gmail.com>
2025-05-28 20:20:32 -0400

    Macro Functions

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,11 +3,44 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    BROWSER = SAFE_RANGE,
+    CALC,
+    MUTE,
+};
+
+void run(const char *command){
+    register_code(KC_LGUI);
+    tap_code(KC_R);
+    unregister_code(KC_LGUI);
+    wait_ms(100);
+
+    send_string(command);
+    tap_code(KC_ENTER);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record){
+    if (record->event.pressed){
+        switch (keycode) {
+            case BROWSER:
+                run("vivaldi");
+                return false;
+            case CALC:
+                run("calc");
+                return false;
+            case MUTE:
+                tap_code(KC_AUDIO_MUTE);
+                return false;
+        }
+    }
+    return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_ortho_3x3(
-        KC_1,   KC_2,   KC_3,
+        BROWSER,   CALC,   KC_3,
         KC_4,   KC_5,   KC_6,
-        KC_7,   KC_8,   KC_9
+        KC_7,   KC_8,   MUTE
     )
-};7777
+};
